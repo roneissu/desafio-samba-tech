13f99d58f00ff7e898e519d2c66d1fca32490200
Ronaldo Luis <sadeluisronaldo@gmail.com>
2019-08-14 10:26:40 -0300

    Updates

---
+++ precache-manifest.31bfb23c9044a54eccdd6a0c2c666bfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385bd804a609de9da18be2d4e1619d63",
+    "url": "/desafio-samba-tech/index.html"
+  },
+  {
+    "revision": "2937e5dd744601e9baf8",
+    "url": "/desafio-samba-tech/static/css/main.d1ed511f.chunk.css"
+  },
+  {
+    "revision": "d003ba02e601504474e1",
+    "url": "/desafio-samba-tech/static/js/2.9be8c276.chunk.js"
+  },
+  {
+    "revision": "2937e5dd744601e9baf8",
+    "url": "/desafio-samba-tech/static/js/main.31c6f52a.chunk.js"
+  },
+  {
+    "revision": "cea66aa5deac5267b516",
+    "url": "/desafio-samba-tech/static/js/runtime~main.f94f0692.js"
+  }
+]);
